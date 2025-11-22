fcbc265632769367c5a33d12c8520859206097e9
Jacob Asper <jacobasper191@gmail.com>
2025-11-22 17:41:31 -0500

    Update to print to stdout and stderr debug print. Move cli tests to test dir

---
+++ tests/parse_failure.rs
@@ -0,0 +1,43 @@
+use annotate_snippets::{Renderer, renderer::DecorStyle};
+use insta::assert_snapshot;
+use jjpwrgem::test_json::*;
+use rstest::rstest;
+
+macro_rules! fixture_tuple {
+    ($const:ident) => {
+        (stringify!($const), $const)
+    };
+}
+
+#[rstest]
+#[case(fixture_tuple!(OBJECT_MISSING_COLON_WITH_COMMA))]
+#[case(fixture_tuple!(OBJECT_MISSING_COLON_WITH_LEADING_WHITESPACE))]
+#[case(fixture_tuple!(OBJECT_MISSING_COLON_WITH_NULL))]
+#[case(fixture_tuple!(OBJECT_MISSING_COLON_WITH_CLOSED_CURLY))]
+#[case(fixture_tuple!(OBJECT_MISSING_COLON))]
+#[case(fixture_tuple!(OBJECT_MISSING_VALUE))]
+#[case(fixture_tuple!(OBJECT_MISSING_COMMA_BETWEEN_VALUES))]
+#[case(fixture_tuple!(OBJECT_MISSING_COMMA_OR_CLOSING_WITH_WHITESPACE))]
+#[case(fixture_tuple!(OBJECT_TRAILING_COMMA_WITH_CLOSED))]
+#[case(fixture_tuple!(OBJECT_TRAILING_COMMA))]
+#[case(fixture_tuple!(OBJECT_DOUBLE_OPEN_CURLY))]
+#[case(fixture_tuple!(OBJECT_OPEN_CURLY))]
+#[case(fixture_tuple!(CLOSED_CURLY))]
+#[case(fixture_tuple!(EMPTY_INPUT))]
+// #[case(fixture_tuple!(DOUBLE_QUOTE))]
+#[case(fixture_tuple!(OBJECT_WITH_LINE_BREAK_VALUE))]
+#[case(fixture_tuple!(OBJECT_WITH_ADJACENT_STRINGS))]
+#[case(fixture_tuple!(OBJECT_EMPTY_THEN_OPEN))]
+#[case(fixture_tuple!(UNEXPECTED_CHARACTER))]
+#[case(fixture_tuple!(UNEXPECTED_ESCAPED_CHARACTER))]
+fn annotate_test_json_fixtures_snapshots(#[case] (name, json): (&str, &str)) {
+    use jjpwrgem::cli::run;
+
+    let renderer = Renderer::plain().decor_style(DecorStyle::Ascii);
+    let annotated = run(json, &renderer);
+
+    assert_snapshot!(
+        name.to_ascii_lowercase(),
+        format_output_snapshot(json, &annotated)
+    );
+}
